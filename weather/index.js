dc9091a141022cade19c26973b6e6b8013bba6ba
Martin <martin2jakovoski@gmail.com>
2026-02-25 01:36:10 +0100

    ogne

---
+++ weather/index.js
@@ -0,0 +1,6 @@
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "d66e96ba52c38c49b593f68b1450243b";
+
+weatherForm.addEventListener("submit", event => {
