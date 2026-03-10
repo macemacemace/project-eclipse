791ec3a9406aaa9438db3f6347627b0ab8ee022c
Martin <martin2jakovoski@gmail.com>
2026-03-11 00:20:55 +0100

    dodaj gi utre site data

--- reactJavas/src/SummonerPage.jsx
+++ reactJavas/src/SummonerPage.jsx
@@ -24,6 +24,7 @@ const SummonerPage = () =>{
    
    const data = await response.json()
     
+   console.log(data)
     
       setSummonerData(data);
     }
@@ -32,7 +33,39 @@ const SummonerPage = () =>{
 
 
   return(
+    <div>
     <div>{summonerData?.data?.gameName}</div>
+    <div>{summonerData?.data?.tagLine}</div>
+    <div>{summonerData?.data2?.[0]?.tier}</div>
+    <div>{summonerData?.data2?.[0]?.rank}</div>
+    <div>W:{summonerData?.data2?.[0]?.wins}</div>
+    <div>L:{summonerData?.data2?.[0]?.losses}</div>
+
+    {summonerData?.matchesArray.map((match,index) => (
+      <div>
+      <div>Game Duration:{match.gameDuration}</div>
+
+
+      <div>Champ name:{match.championsArray[0]}</div>
+      <div style={{display: 'flex'}}>
+        <div>KDA:{match.playerKillsArray[0]}/</div>
+        <div>{match.playerDeathsArray[0]}/</div>
+        <div>{match.playerAssistsArray[0]}</div>
+        
+        </div>
+      
+      <div>DamageDealt:{match.damageDealtArray[0]}</div>
+      <div>Key stone:{match.keyStonesArray[0]}</div>
+      <div>Primary rune 1:{match.keyRune1Array[0]}</div>
+      <div>Primary rune 2:{match.keyRune2Array[0]}</div>
+      <div>Primary rune 3:{match.keyRune3Array[0]}</div>
+      <div>CS:{match.minionKillsArray[0]}</div>
+      
+
+      </div>//end of inside map div
+    ))}
+
+   </div> // end of main div
     
 
   )
