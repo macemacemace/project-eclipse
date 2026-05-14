5780be6f0a556ae0e2fe2c3163c11f987d90071c
Martin <martin2jakovoski@gmail.com>
2026-05-14 18:33:53 +0200

    ogen

--- reactJavas/src/MatchCard.jsx
+++ reactJavas/src/MatchCard.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useParams} from "react-router-dom"
 import {useState} from "react"
+import './SummonerPage.css'
 
 
 
 
-const MatchCard = ({ match, playerIndex, spellData, runesData, getSpellName, getRuneName, name, tag }) =>{
+const MatchCard = ({ match, playerIndex, spellData, runesData, getSpellName, getRuneName, name, tag, version }) =>{
 
     const[isOpen, setIsOpen] = useState(false);
   const kills = match.playerKillsArray[playerIndex];
@@ -27,7 +28,7 @@ const MatchCard = ({ match, playerIndex, spellData, runesData, getSpellName, get
 
       const csPerMin = (Cs / gameDur1).toFixed(1);
 
-    const roles = ['Top', 'Jungle', 'Mid', 'ADC', 'Support', 'Top', 'Jungle', 'Mid', 'ADC', 'Support'];
+    const roles = ['Top', 'Jungle', 'Mid', 'Bottom', 'Support', 'Top', 'Jungle', 'Mid', 'Bottom', 'Support'];
         
 
 
@@ -39,10 +40,10 @@ const MatchCard = ({ match, playerIndex, spellData, runesData, getSpellName, get
             {match.winningTeam[playerIndex] ? "WIN" : "LOSS"}
         </div>
         <img className="KeyStone" src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyStonesArray[playerIndex], runesData)?.icon}`} alt="keystone"/>
-        <img className="ChampIcon" src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/champion/${match.championsArray[playerIndex]}.png`} alt="champ icon" style={{width: '60px', height: '60px'}}/>
+        <img className="ChampIcon" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${match.championsArray[playerIndex]}.png`} alt="champ icon" style={{width: '60px', height: '60px'}}/>
         <div className="Summoners">
-            <img className="Summoner1" src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/spell/${spellData && getSpellName(match.summoner1Array[playerIndex], spellData)?.id}.png`} />
-            <img className="Summoner2" src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/spell/${spellData && getSpellName(match.summoner2Array[playerIndex], spellData)?.id}.png`} />
+            <img className="Summoner1" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${spellData && getSpellName(match.summoner1Array[playerIndex], spellData)?.id}.png`} />
+            <img className="Summoner2" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${spellData && getSpellName(match.summoner2Array[playerIndex], spellData)?.id}.png`} />
         </div>
         <div className="Role">{roles[playerIndex]}</div>
         <div className="GameStats">
@@ -67,18 +68,120 @@ const MatchCard = ({ match, playerIndex, spellData, runesData, getSpellName, get
             ].map((item, i) => (
                 <div key={i} className="itemSlot">
                     {item !== 0 && item !== null
-                        ? <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${item}.png`} alt="item" />
+                        ? <img src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item}.png`} alt="item"
+                        
+        
+                        />
                         : null}
                 </div>
             ))}
         </div>
         <div className="itemSlot ward">
             {match.playerBuildsArray6[playerIndex] !== 0
-                ? <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray6[playerIndex]}.png`} alt="ward" />
+                ? <img src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${match.playerBuildsArray6[playerIndex]}.png`} alt="ward" />
                 : null}
         </div>
         <div className="GameDuration">{gameDur1}m</div>
         <div className={`dropdownBtn ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>▼</div>
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {isOpen && (
+                <div className="dropdown">
+                    {isOpen && (
+    <div className="dropdown">
+        <div className="teamLabel blue">Blue Team</div>
+        {Array.from({length: 5}).map((_, i) => (
+            <div key={i} className={`playerRow ${i === playerIndex ? "me" : ""}`}>
+                <img className="miniChamp" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${match.championsArray[i]}.png`} alt="champ"/>
+                <div className="playerName">{match.riotIdGameNamesArray[i]}</div>
+                <div className="playerKda">{match.playerKillsArray[i]}/{match.playerDeathsArray[i]}/{match.playerAssistsArray[i]}</div>
+                <div className="playerCs">CS {match.minionKillsArray[i]}</div>
+                <div className="miniItems">
+                    {[match.playerBuildsArray0[i], match.playerBuildsArray1[i], match.playerBuildsArray2[i],
+                      match.playerBuildsArray3[i], match.playerBuildsArray4[i], match.playerBuildsArray5[i]
+                    ].map((item, j) => (
+                        <div key={j} className="miniItemSlot">
+                            {item !== 0 && item !== null
+                                ? <img src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item}.png`} alt=""/>
+                                : null}
+                        </div>
+                    ))}
+                </div>
+                
+                <div className="runes">
+    <div className="runeKeystone">
+        <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyStonesArray[i], runesData)?.icon}`} alt="keystone"/>
+    </div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune1Array[i], runesData)?.icon}`} alt="rune1"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune2Array[i], runesData)?.icon}`} alt="rune2"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune3Array[i], runesData)?.icon}`} alt="rune3"/></div>
+    <div className="runeSep">|</div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune1Array[i], runesData)?.icon}`} alt="rune4"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune2Array[i], runesData)?.icon}`} alt="rune5"/></div>
+</div>
+            </div>
+        ))}
+
+        <div className="teamLabel red">Red Team</div>
+        {Array.from({length: 5}).map((_, i) => {
+            const ri = i + 5;
+            console.log(getRuneName(match.keyStonesArray[0], runesData)?.icon)
+            return (
+                <div key={ri} className={`playerRow ${ri === playerIndex ? "me" : ""}`}>
+                    <img className="miniChamp" src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${match.championsArray[ri]}.png`} alt="champ"/>
+                    <div className="playerName">{match.riotIdGameNamesArray[ri]}</div>
+                    <div className="playerKda">{match.playerKillsArray[ri]}/{match.playerDeathsArray[ri]}/{match.playerAssistsArray[ri]}</div>
+                    <div className="playerCs">CS {match.minionKillsArray[ri]}</div>
+                    <div className="miniItems">
+                        {[match.playerBuildsArray0[ri], match.playerBuildsArray1[ri], match.playerBuildsArray2[ri],
+                          match.playerBuildsArray3[ri], match.playerBuildsArray4[ri], match.playerBuildsArray5[ri]
+                        ].map((item, j) => (
+                            <div key={j} className="miniItemSlot">
+                                {item !== 0 && item !== null
+                                    ? <img src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item}.png`} alt="item"/>
+                                    : null}
+                            </div>
+                        ))}
+                    </div>
+                    
+                    <div className="runes">
+                     <div className="runeKeystone">
+                       <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyStonesArray[i], runesData)?.icon}`} alt="keystone"/>
+                     </div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune1Array[i], runesData)?.icon}`} alt="rune1"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune2Array[i], runesData)?.icon}`} alt="rune2"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune3Array[i], runesData)?.icon}`} alt="rune3"/></div>
+    <div className="runeSep">|</div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune1Array[i], runesData)?.icon}`} alt="rune4"/></div>
+    <div className="runeSm"><img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune2Array[i], runesData)?.icon}`} alt="rune5"/></div>
+</div>
+                </div>
+            )
+        })}
+    </div>
+)}
+
+
+                </div>
+            )}
+
+
     </div>
 )
 
--- reactJavas/src/SummonerPage.jsx
+++ reactJavas/src/SummonerPage.jsx
@@ -11,18 +11,24 @@ const SummonerPage = () =>{
   const {region, name, tag} = useParams()
   console.log(region, name, tag)
 
+  
 
   const [summonerData, setSummonerData] = useState(null);
   const [spellData, setSpellData] =useState(null);
   const [runesData, setRunesData] = useState(null);
-   
+  const [version, setVersion] = useState(null);
   useEffect(() => {
     async function fetchData() {
+
+      const versionRes = await fetch("https://ddragon.leagueoflegends.com/api/versions.json");
+        const versions = await versionRes.json();
+        const latestVersion = versions[0];
+        setVersion(latestVersion);
       
     
    const response = await fetch(`http://localhost:3000/summoner/${region}/${name}/${tag}`)
-   const spellJson = await fetch("https://ddragon.leagueoflegends.com/cdn/16.5.1/data/en_US/summoner.json");
-   const runesJson = await fetch (`https://ddragon.leagueoflegends.com/cdn/16.5.1/data/en_US/runesReforged.json`)
+   const spellJson = await fetch(`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/data/en_US/summoner.json`);
+   const runesJson = await fetch (`https://ddragon.leagueoflegends.com/cdn/${latestVersion}/data/en_US/runesReforged.json`)
    if(!response.ok){
       throw new Error("cant fetch data")
     }
@@ -168,6 +174,7 @@ console.log(summonerData?.data5)
             runesData={runesData}
             getSpellName={getSpellName}
             getRuneName={getRuneName}
+            version={version}
         />
        
       
@@ -199,77 +206,4 @@ export default SummonerPage;
 
  
 
-{/*<div className="Matches">
-    {summonerData?.matchesArray.map((match,index) => (
-      <div>
-      <div>Game Duration:{match.gameDuration}</div>
-
-      {Array.from({length: 10}).map((_, playerIndex) => (
-
-
-        <div key = {playerIndex}>
-      <div>---------------Summoner Name:{match.riotIdGameNamesArray[playerIndex]}-----------------------</div>
-       <div>tag:{match.riotIdTagLinesArray[playerIndex]}</div>
-      <div>Champ name:{match.championsArray[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/champion/${match.championsArray[playerIndex]}.png`} alt="champ icon"  style={{width: '50px', height: '50px'}}/>
-      console.log()
-      <div style={{display: 'flex'}}>
-        <div>KDA:{match.playerKillsArray[playerIndex]}/</div>
-        <div>{match.playerDeathsArray[playerIndex]}/</div>
-        <div>{match.playerAssistsArray[playerIndex]}</div>
-        
-        </div>
-      
-      <div>DamageDealt:{match.damageDealtArray[playerIndex]}</div>
-      <div>Key stone:{match.keyStonesArray[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyStonesArray[playerIndex], runesData)?.icon}`} alt = "keystone"/>
-      <div>Primary rune 1:{match.keyRune1Array[playerIndex]}</div>
-       <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune1Array[playerIndex], runesData)?.icon}`} alt = "keystone1"/>
-      <div>Primary rune 2:{match.keyRune2Array[playerIndex]}</div>
-       <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune2Array[playerIndex], runesData)?.icon}`} alt = "keystone2"/>
-      <div>Primary rune 3:{match.keyRune3Array[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.keyRune3Array[playerIndex], runesData)?.icon}`} alt = "keystone3"/>
-      <div>second rune tree:{match.secondaryRuneName[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRuneName[playerIndex], runesData)?.icon}`} alt = "secondary tree"/>
-      <div>first secondary rune:{match.secondaryRune1Array[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune1Array[playerIndex], runesData)?.icon}`} alt = "keystone2"/>
-    <div>second secondary rune:{match.secondaryRune2Array[playerIndex]}</div>
-    <img src={`https://ddragon.leagueoflegends.com/cdn/img/${getRuneName(match.secondaryRune2Array[playerIndex], runesData)?.icon}`} alt = "keystone2"/>
-    <div>first shard:{match.playerShard1Array[playerIndex]}</div>
-    <div>second shard:{match.playerShard2Array[playerIndex]}</div>
-    <div>third shard:{match.playerShard3Array[playerIndex]}</div>
-      <div>CS:{match.minionKillsArray[playerIndex]}</div>
-      <div>Level:{match.playerLevelArray[playerIndex]}</div>
-
-      
-      <div>first item:{match.playerBuildsArray0[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray0[playerIndex]}.png`} alt="first item" style ={{width: '50px', height: '50px'}}/>
-      <div>second item:{match.playerBuildsArray1[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray1[playerIndex]}.png`} alt="second item" style ={{width: '50px', height: '50px'}}/>
-      <div>third item:{match.playerBuildsArray2[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray2[playerIndex]}.png`} alt="third item" style ={{width: '50px', height: '50px'}}/>
-      <div>forth item:{match.playerBuildsArray3[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray3[playerIndex]}.png`} alt="forth item" style ={{width: '50px', height: '50px'}}/>
-      <div>fifth item:{match.playerBuildsArray4[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray4[playerIndex]}.png`} alt="fifth item" style ={{width: '50px', height: '50px'}}/>
-      <div>sixt item:{match.playerBuildsArray5[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray5[playerIndex]}.png`} alt="sixth item" style ={{width: '50px', height: '50px'}}/>
-      <div>sevent item:{match.playerBuildsArray6[playerIndex]}</div>
-        <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/item/${match.playerBuildsArray6[playerIndex]}.png`} alt="seveth item" style ={{width: '50px', height: '50px'}}/>
-
-        
-      <div>first summoner:{match.summoner1Array[playerIndex]}</div>
-      <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/spell/${spellData && getSpellName(match.summoner1Array[playerIndex], spellData)?.id}.png`} />
-      <div>second summoner:{match.summoner2Array[playerIndex]}</div>
-        <img src={`https://ddragon.leagueoflegends.com/cdn/16.5.1/img/spell/${spellData && getSpellName(match.summoner2Array[playerIndex], spellData)?.id}.png`} />
-
-
-      <div>did he win?{match.winningTeam[playerIndex] ? "Win" : "Loss"}</div>
-        
-        </div> 
-       
-        ))}
-      </div>
-      
-    ))}
-    </div> */}
+{/* */}
